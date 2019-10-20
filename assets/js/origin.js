5b902819adebd8dd258bb5e0dd37c448717fe45f
iyoka <kato-hiroto@ed.tmu.ac.jp>
2019-10-21 05:09:29 +0900

    ハッカソンエピソード記載

--- assets/js/origin.js
+++ assets/js/origin.js
@@ -4,7 +4,7 @@ function isShowButton(index) {
 	if (index <= 0) {
 		chi[0].style.visibility = "hidden";
 		chi[1].style.visibility = "visible";
-	} else if (index >= 5) {
+	} else if (index >= 7) {
 		chi[0].style.visibility = "visible";
 		chi[1].style.visibility = "hidden";
 	} else {
